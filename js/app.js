c9e278ea0c62979e6f1dd63d0f0546f65799d37d
LucasAmmer <lucas@ammer.nl>
2023-06-16 21:04:15 +0200

    updated base to be custom

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
 const container = document.getElementById("container");
 container.innerText = "Javascript is working";
-const ServiceWorkerURL = "/pwa-sample/sw.js"
+const ServiceWorkerURL = "/greenit/sw.js";
 const registerServiceWorker = async () => {
-    if ("serviceWorker" in navigator) {
-      try {
-        const registration = await navigator.serviceWorker.register(ServiceWorkerURL, {
-          scope: "/pwa-sample/",
-        });
-        if (registration.installing) {
-          console.log("Service worker installing");
-        } else if (registration.waiting) {
-          console.log("Service worker installed");
-        } else if (registration.active) {
-          console.log("Service worker active");
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        ServiceWorkerURL,
+        {
+          scope: "/greenit/",
         }
-      } catch (error) {
-        console.error(`Registration failed with ${error}`);
+      );
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
       }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
     }
+  }
 };
 
 registerServiceWorker();
-  
--- sw.js
+++ sw.js
@@ -1,24 +1,25 @@
-const appName = "sample pwa app";
-const root = "/pwa-sample"
-const assets = [    // all files that will be cached
-    `${root}/`,
-    `${root}/index.html`,
-    `${root}/js/app.js`,
-    `${root}/css/style.css`,
-]
+const appName = "GreenIt";
+const root = "/greenit";
+const assets = [
+  // all files that will be cached
+  `${root}/`,
+  `${root}/index.html`,
+  `${root}/js/app.js`,
+  `${root}/css/style.css`,
+];
 
 self.addEventListener("install", (InstallEvent) => {
-    InstallEvent.waitUntil(
-        caches.open(appName).then((cache) => {
-            cache.addAll(assets);
-        })
-    )
-})
+  InstallEvent.waitUntil(
+    caches.open(appName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-})
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
